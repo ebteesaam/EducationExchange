10125447809cc44c588bfe4518742d147425e90b
Ebtesam Harith <ebtesam@github.com>
2018-02-07 23:55:59 +0300

    after adding database into firebase and login the user after verified the email

--- app/src/main/java/com/example/ebtesam/educationexchange/profile/ProfilePage.java
+++ app/src/main/java/com/example/ebtesam/educationexchange/profile/ProfilePage.java
@@ -13,7 +13,7 @@ public class ProfilePage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile_page);//
+        setContentView(R.layout.activity_profile_page);
 
         Button editProfile=findViewById(R.id.edit_user);
         editProfile.setOnClickListener(new View.OnClickListener() {
